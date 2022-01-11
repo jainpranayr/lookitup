ffeb499392626e830557139dc42f8d5993b93fdd
Pranay Jain <jpranay688@gmail.com>
2022-01-12 00:09:34 +0530

    add dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
+import { DarkModeToggle } from "./components"
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <div className='App'>
-      <p className='text-3xl font-bold underline'>Hello World!</p>
+    <div className={darkMode ? "dark" : ""}>
+      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+        Hello, World
+        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+      </div>
     </div>
   )
 }
