98801d79284b7114cbb6754d618e6c69b9d5b73a
Pranay Jain <jpranay688@gmail.com>
2022-01-12 16:52:24 +0530

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+
 import { DarkModeToggle } from "./utils"
 
 const Navbar = ({ darkMode, setDarkMode }) => {
@@ -6,9 +7,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
     <div className='p-3 flex flex-wrap justify-center sm:justify-between items-center border-b dark:border-gray-700 border-gray-200'>
       <div className='flex justify-between items-center space-x-5 w-screen xl:px-40 md:px-20'>
         <Link to='/'>
-          <p className='text-2xl font-bold'>
-            LOOK<span className='text-blue-700 ml-1'>UP</span>
-          </p>
+          <p className='text-2xl font-extrabold'>lookitup</p>
         </Link>
         <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
