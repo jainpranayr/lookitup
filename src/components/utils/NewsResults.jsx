0f061dbd6623eca76b58117c94f2fa28bbe56a0a
Pranay Jain <jpranay688@gmail.com>
2022-02-05 12:45:18 +0530

    add newsResults component

--- src/components/utils/NewsResults.jsx
+++ src/components/utils/NewsResults.jsx
@@ -1,5 +1,36 @@
+import { useResultContext } from "../../contexts/ResultContextProvider"
+import { useViewportContext } from "../../contexts/ViewportContextProvider"
+import { truncate } from "./truncate"
+
 const NewsResults = () => {
-  return <div>News Results will appear here</div>
+  const { results } = useResultContext()
+  const { width } = useViewportContext()
+
+  return (
+    <div className='flex flex-wrap space-y-6 xl:px-40 md:px-20'>
+      {results?.map(({ links, id, source, title }) => (
+        <div
+          key={id}
+          className='w-full bg-white dark:bg-gray-800 p-5 rounded-2xl hover:shadow-sm'
+        >
+          <a href={links[0].href} target='_blank' rel='noreferrer'>
+            <p className='text-sm'>{truncate(links[0].href, 30)}</p>
+            <h1 className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+              {width <= 400 ? truncate(title, 60) : title}
+            </h1>
+          </a>
+          <a
+            href={source.href}
+            target='_blank'
+            rel='noreferrer'
+            className='hover:underline text-sm'
+          >
+            {source.title}
+          </a>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default NewsResults
