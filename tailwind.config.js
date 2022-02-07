327ec6e9c7493fc6b1ebf9131c8709f8ae824216
Pranay Jain <jpranay688@gmail.com>
2022-02-07 10:43:55 +0530

    fix tailwind dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class",
   theme: {
-    extend: {
-      screens: {
-        dark: { raw: "(prefers-color-scheme: dark)" },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
