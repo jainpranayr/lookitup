072fb2954360091dda63616109cd8b41fc1355b1
Pranay Jain <jpranay688@gmail.com>
2022-07-21 01:03:39 +0530

    🐛 image endpoint

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,31 +1,31 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { createContext, useContext, useState } from 'react'
 
 const ResultContext = createContext()
-const baseUrl = "https://google-search3.p.rapidapi.com/api/v1"
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
 
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState('')
 
   const getResults = async url => {
     setLoading(true)
 
     const res = await fetch(`${baseUrl}${url}`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "x-user-agent": "desktop",
-        "x-proxy-location": "EU",
-        "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+        'x-user-agent': 'desktop',
+        'x-proxy-location': 'EU',
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
       },
     })
 
     const data = await res.json()
 
-    if (url.includes("/news")) {
+    if (url.includes('/news')) {
       setResults(data.entries)
-    } else if (url.includes("/images")) {
+    } else if (url.includes('/image')) {
       setResults(data.image_results)
     } else {
       setResults(data.results)
@@ -36,8 +36,7 @@ export const ResultContextProvider = ({ children }) => {
 
   return (
     <ResultContext.Provider
-      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
-    >
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
       {children}
     </ResultContext.Provider>
   )
